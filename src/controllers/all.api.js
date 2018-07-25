37f356895e26f4820c1d857f57c9e42d451e3128
bobbychen <lovescaoliting@gmail.com>
2018-07-25 16:40:13 +0800

    add order api to router

--- src/controllers/all.api.js
+++ src/controllers/all.api.js
@@ -5,6 +5,7 @@ import activityRule from './activity-rule';
 import cafeStorage from './cafe-storage';
 import coffeeAssist from './coffee-assist';
 import coffeeWish from './coffee-wish';
+import order from './order';
 
 const router = new express.Router();
 
@@ -18,5 +19,6 @@ router.use(activityRule);
 router.use(cafeStorage);
 router.use(coffeeAssist);
 router.use(coffeeWish);
+router.use(order);
 
 export default router;
