4a3106697fc5c52c4979217621cee6d4ae78388f
hanzhong-yan <center.yan@gmail.com>
2018-08-05 19:20:38 +0800

    add ssl support

--- src/app.js
+++ src/app.js
@@ -5,13 +5,21 @@ import mongoose from './utils/mongoose';
 import webServer from './utils/webserver';
 import routes from './controllers/all.api';
 
+import fs from 'fs';
+
 
 const mongodb = mongoose.init(
     config.dbHosts, config.dbName,
     config.dbUser, config.dbPass, config.dbParams
 );
 
-const app = express();
+//add ssl support
+var privateKey = fs.readFileSync('/etc/letsencrypt/live/wx.harryyan.xin/privkey.pem');
+var certificate = fs.readFileSync('/etc/letsencrypt/live/wx.harryyan.xin/fullchain.pem');
+var credentials = {key: privateKey, cert: certificate};
+var app = express.createServer(credentials);
+
+//const app = express();
 
 app.use('/', routes);
 
